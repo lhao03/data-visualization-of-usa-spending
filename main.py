2a40fcb78ce306a6e68328eb94bc654433fb44dc
Lucy <hao.lucyy@gmail.com>
2020-08-22 08:38:20 -0600

    make label val map

--- main.py
+++ main.py
@@ -6,22 +6,31 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html 
 from dash.dependencies import Input, Output
+from states import state_codes, states_names
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets) 
 
-# import and clean data 
+# import data
 df = pd.read_csv("2004-2017_usa_spending.csv")
+
+def make_mappings():
+    list_of_labels_vals = []
+    list_of_labels_vals.append({"label": "USA", "value": "USA"})
+    for i in range(0, len(state_codes)):
+        label_dict = {"label": states_names[i]}
+        label_dict["value"] = state_codes[i]
+        list_of_labels_vals.append(label_dict)
+    return list_of_labels_vals
 
 # app layout - components 
 app.layout = html.Div([
-    html.H1("State General Expenditures, Per Capita", style={'text-align':'center', 'font-family': 'sans-serif'}),
+    html.H1("State General Expenditures, Per Capita in Dollars", style={'text-align':'center', 'font-family': 'sans-serif'}),
+    
+    # div holding slider 
     html.Div([
- dcc.Slider(id='slct_year',
-    min=2005,
-    max=2017,
-    step=None, 
-    marks={
+        # slider for usa map
+        dcc.Slider(id='slct_year', min=2005, max=2017, step=None, marks={
         2005: '2005',
         2006: '2006',
         2007: '2007',
@@ -35,12 +44,10 @@ app.layout = html.Div([
         2015: '2015',
         2016: '2016',
         2017: '2017'
-    },
-    value=2005    
-    ),
+    }, value=2005),
     
-     dcc.Dropdown(id='slct_fndng',
-    options=[
+    # drop down for usa map   
+        dcc.Dropdown(id='slct_fndng', options=[
         {"label": "Total Spending", "value": "total"},
         {"label": "Elementary and Secondary Education", "value": "elementary_and_secondary_edu"},
         {"label": "Higher Education", "value": "higher_edu"},
@@ -49,43 +56,34 @@ app.layout = html.Div([
         {"label": "Highways", "value": "highways"},
         {"label": "Police", "value": "police"},
         {"label": "Other", "value": "all_other"},
-        {"label": "Population", "value": "population_thousands"}],
-        
-        multi=False,
-        value="total",
-        style={'width': "70%"}
-        ),
+        {"label": "Population", "value": "population_thousands"}], multi=False,
+        value="total", style={'width': "70%"}),
          dcc.Graph(id='usa_map', figure={}) 
         
-    ], 
-    style={
+    ], style={
         # 'width': '48%', 
         # 'display': 'inline-block'
-        }
-    ),
+        }),
 
-        html.Div([
-       
-        dcc.Graph(id='state_figure', figure={}, ) 
-    ],
+    # div holding graphs that are state specific 
+    html.Div([
+        dcc.Dropdown(id='slct_state', options=make_mappings()),
+
+        dcc.Graph(id='state_figure', figure={},)], 
         style={
             'width': '95%',
             #  'align': 'right',
               'display': 'inline-block'
-              }
-    ),
-
-            html.Div([
-            html.H1("Per State", style={'text-align':'center', 'font-family': 'sans-serif'}),
-        ]),
-], style={'background-color': '#8CABBE', 'columnCount': 2})
+    }),
+], style={'background-color': '#A4BFEB', 'columnCount': 2})
 
 # connect using the callbacks
 @app.callback(
     [Output(component_id='usa_map', component_property='figure'),
     Output(component_id='state_figure', component_property='figure')],
     [Input(component_id='slct_year', component_property='value'),
-    Input(component_id='slct_fndng', component_property='value')]
+    Input(component_id='slct_fndng', component_property='value'),
+    Input(component_id='slct_state', component_property='value')]
 )
 def update_graph(year, funding):
     # option_slctd refers to value
@@ -93,8 +91,8 @@ def update_graph(year, funding):
     df_copy = df.copy()
     df_year = df_copy[df_copy["year"] == year]
 
-    
-    fig = px.choropleth(
+    # year by year     
+    fig_usa = px.choropleth(
         data_frame=df_year, 
         locationmode='USA-states',
         locations='status_code',
@@ -103,16 +101,26 @@ def update_graph(year, funding):
         color_continuous_scale="Viridis"
     )
 
-    figg= px.choropleth(
-        data_frame=df_year, 
-        locationmode='USA-states',
-        locations='status_code',
-        scope='usa',
-        color=funding,
-        color_continuous_scale="Viridis"
-    )
+    # specific state or compare all of the states by average
+    fig_state= make_subplots(rows=3, cols=1,
+    subplot_titles=("Plot 1", "Plot 2", "Plot 3", "Plot 4"))
 
-    return [fig, figg] # the outputs
+    fig_state.append_trace(go.Scatter(
+    x=[3, 4, 5],
+    y=[1000, 1100, 1200],
+    ), row=1, col=1)
+
+    fig_state.append_trace(go.Scatter(
+    x=[2, 3, 4],
+    y=[100, 110, 120],
+    ), row=2, col=1)
+
+    fig_state.append_trace(go.Scatter(
+    x=[0, 1, 2],
+    y=[10, 11, 12]
+    ), row=3, col=1)
+
+    return [fig_usa, fig_state] # the outputs
 
 # run the app 
 if __name__ == "__main__":
