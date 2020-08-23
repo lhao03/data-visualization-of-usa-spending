a40481815bf41f38a63d35dda970936d2f0c78af
Lucy <hao.lucyy@gmail.com>
2020-08-23 00:27:53 -0600

    idk tried to fix input

--- inputs.py
+++ inputs.py
@@ -11,22 +11,29 @@ def norm(x):
     norm = pd.read_csv('stats_for_norm.csv')
     return (x - norm['mean']) / norm['std']
 
-def to_dataframe(input):
+def to_dataframe(input_raw):
     # inputs should be dict or list
     vals = ['region', 'total','elementary_and_secondary_edu', 'higher_edu' ,'public_welfare','health_and_hospitals','highways','police','all_other' ,'population_thousands','year',
-     	'status_code','hispanic','white','black','asian','indigneous']
-
+     'status_code','hispanic','white','black','asian','indigneous']
+    
+    dict_to_df = dict(zip(vals, list(input_raw)))
+    for key,val in dict_to_df.items():
+        dict_to_df[key] = [val]
     # turn inputs into pandas dataframe
-    df = pd.DataFrame(columns=vals)
-    df.loc[0] = input
-
-    # one hot encode 
+    df = pd.DataFrame.from_dict(dict_to_df)
+    
+     # one hot encode 
     one_hot = generate_mappings_states()
     df['region'] = df['region'].map(one_hot)
     df = pd.get_dummies(df, prefix='', prefix_sep='')
 
-    # normalise 
-    norm_df = norm(df)
+#     # normalise 
+#     norm_df = norm(df)
+#     print(df)
 
     # return 
-    return norm_df
+    return df
+
+test = ['Connecticut',9429.969727,2406.116699,726.015503,1729.546021,575.013611,600.278748,311.752289,3081.246338,3594.915039,2013,'CT',16.1,66.7,9.9,4.5,0.2]
+pandas_df = to_dataframe(test)
+print(pandas_df)
--- model.py
+++ model.py
@@ -6,3 +6,30 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+checkpoint_path = "hack-for-people/cp.ckpt"
+
+
+def build_model():
+    model = keras.Sequential([
+    layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1)
+  ])
+    
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    
+    model.compile(optimizer='adam', loss='mse', metrics=['mae', 'mse'])
+#     model.compile(loss='mse',
+#                 optimizer=optimizer,
+#                 metrics=['mae', 'mse', 'accuracy'])
+    
+    return model
+
+# build model
+model = build_model()
+model.load_weights(checkpoint_path)
+# test predictions
+
+normed_test_data = 
+
+test_predictions = model.predict(normed_test_data)
